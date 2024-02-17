a39ab3c4ad4a066917a848ebe123041896648b21
Alexandr <alex.yermakov20@gmail.com>
2024-02-17 16:48:27 -0600

    driver for magnetometer

---
+++ adcs-tsat6-stm32project-boardV1/Drivers/Hardware_Peripherals/Inc/magnetometer_driver.h
@@ -0,0 +1,44 @@
+/*
+ * magnetometer_driver.h
+ *
+ *  Created on: Jan 19, 2024
+ *      Author: Alexandr Yermakov
+ */
+
+/*
+
+Datasheet URL:
+https://media.digikey.com/pdf/Data%20Sheets/MEMSIC%20PDFs/MMC5983MA_RevA_4-3-19.pdf
+*/
+
+#ifndef HARDWARE_PERIPHERALS_INC_MAGNETOMETER_DRIVER_H_
+#define HARDWARE_PERIPHERALS_INC_MAGNETOMETER_DRIVER_H_
+
+#include "stm32l4xx_hal.h"
+
+#define MAG_XOUT_L 0x00
+#define MAG_XOUT_H 0x01
+#define MAG_YOUT_L 0x02
+#define MAG_YOUT_H 0x03
+#define MAG_ZOUT_L 0x04
+#define MAG_ZOUT_H 0x05
+#define MAG_TEMP_L 0x06
+#define MAG_TEMP_H 0x07
+#define MAG_STATUS 0x08
+#define MAG_CONTROL_0 0x09
+
+// Function to write to MAGNETOMETER register
+void MAG_WriteReg(uint8_t reg, uint8_t data);
+
+// Function to read from MAGNETOMETER register
+uint8_t MAG_ReadReg(uint8_t reg);
+
+// Function to read magnetic field data
+// pData: Pointer to an array where the magnetic field data will be stored
+// The array has 3 elements for X, Y, and Z
+void MAG_ReadMagneticField(int *pData);
+
+// Function to initialize MAGNETOMETER
+void MAG_Init(void);
+
+#endif
---
+++ adcs-tsat6-stm32project-boardV1/Drivers/Hardware_Peripherals/Src/magnetometer_driver.c
@@ -0,0 +1,52 @@
+#include <magnetometer_driver.h>
+#include "stm32l4xx_hal.h"
+#include <stdio.h>
+#include "main.h"
+
+extern SPI_HandleTypeDef hspi2;
+
+
+// Function to write to MAGNETOMETER register
+void MAG_WriteReg(uint8_t reg, uint8_t data) {
+	HAL_GPIO_WritePin(MAG2_nCS_GPIO_Port, MAG2_nCS_Pin, GPIO_PIN_RESET);
+	uint8_t txData[2] = {reg, data};
+	HAL_SPI_Transmit(&hspi2, txData, 2, 100);
+	HAL_GPIO_WritePin(MAG2_nCS_GPIO_Port, MAG2_nCS_Pin, GPIO_PIN_SET);
+}
+
+// Function to read from MAGNETOMETER register
+uint8_t MAG_ReadReg(uint8_t reg) {
+	HAL_GPIO_WritePin(MAG2_nCS_GPIO_Port, MAG2_nCS_Pin, GPIO_PIN_RESET);
+	uint8_t txData = 0x80 | reg; // MSB must be set for read
+	uint8_t rxData;
+	HAL_SPI_Transmit(&hspi2, &txData, 1, 100);
+	HAL_SPI_Receive(&hspi2, &rxData, 1, 100);
+	HAL_GPIO_WritePin(MAG2_nCS_GPIO_Port, MAG2_nCS_Pin, GPIO_PIN_SET);
+	return rxData;
+}
+
+// Function to read magnetic field data
+void MAG_ReadMagneticField(int *pData) {
+	uint8_t tmpbuffer[6] = {0};
+
+	tmpbuffer[0] = MAG_ReadReg(MAG_XOUT_L);
+	tmpbuffer[1] = MAG_ReadReg(MAG_XOUT_H);
+	tmpbuffer[2] = MAG_ReadReg(MAG_YOUT_L);
+	tmpbuffer[3] = MAG_ReadReg(MAG_YOUT_H);
+	tmpbuffer[4] = MAG_ReadReg(MAG_ZOUT_L);
+	tmpbuffer[5] = MAG_ReadReg(MAG_ZOUT_H);
+
+	pData[0] = (int)(tmpbuffer[1] << 8 | tmpbuffer[0]);
+	pData[1] = (int)(tmpbuffer[3] << 8 | tmpbuffer[2]);
+	pData[2] = (int)(tmpbuffer[5] << 8 | tmpbuffer[4]);
+}
+
+// Function to initialize MAGNETOMETER
+void MAG_Init(void) {
+	// Set CS pin high
+	HAL_GPIO_WritePin(MAG2_nCS_GPIO_Port, MAG2_nCS_Pin, GPIO_PIN_SET);
+	// Configure magnetic sensor settings
+	(MAG_CONTROL_0, 0x00); // Reset sensor
+	HAL_Delay(1); // Wait for reset to complete
+	(MAG_CONTROL_0, 0x01); // Start measurement
+}
